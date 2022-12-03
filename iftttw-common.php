715afb489e9d3ec3161fc954b72e7bb7f01601d9
Joe Harrison <joe.harrison@carpe.io>
2022-12-03 16:53:36 -0500

    add logEntry

--- iftttw-common.php
+++ iftttw-common.php
@@ -30,4 +30,15 @@ function getPayloadOptions(){
     return $payloadArr;
 }
 
+function logEntry($data) {
+
+	global $logFile,$myPid;
+
+	$data = $_SERVER['PHP_SELF']." : [".$myPid."] ".$data;
+	
+	$logWrite= fopen($logFile, "a") or die("Unable to open file!");
+	fwrite($logWrite, date('Y-m-d h:i:s A',time()).": ".$data."\n");
+	fclose($logWrite);
+}
+
 ?>
