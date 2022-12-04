7e1dfff80e510aa4c01997e75b37d89dbaf51908
Joe Harrison <joe.harrison@carpe.io>
2022-12-03 20:51:19 -0500

    Update execute.php

--- commands/execute.php
+++ commands/execute.php
@@ -3,9 +3,15 @@
 include_once "/opt/fpp/www/config.php";
 include_once "/home/fpp/media/plugins/fpp-ifttt-webhooks/iftttw-common.php";
 
+$logFile2 = "/home/fpp/media/logs/debug-execute.log";
+$data2 = "HELLO FROM EXECUTE";
+$logWrite2= fopen($logFile2, "a") or die("Unable to open file!");
+fwrite($logWrite2, date('Y-m-d h:i:s A',time()).": ".$data2."\n");
+fclose($logWrite2);
+
 $eventName = $argv[1];
 $payload = $argv[2];
-logEntry( "Event Name: " . $eventName . "  Payload: " . $payload);
+#logEntry( "Event Name: " . $eventName . "  Payload: " . $payload);
 
 if (strlen($eventName) == 0){
 	throw new Exception('No event specified.');
