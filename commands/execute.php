5a0d7be35c0113833fd46491811ef2114869b704
Joe Harrison <joe.harrison@carpe.io>
2022-12-03 21:44:45 -0500

    Update execute.php

--- commands/execute.php
+++ commands/execute.php
@@ -7,14 +7,7 @@ include_once "/home/fpp/media/plugins/fpp-ifttt-webhooks/iftttw-common.php";
 $eventName = $argv[1];
 $payload = $argv[2];
 
-$logFile2 = "/home/fpp/media/logs/debug-execute.log";
-$data2 = "Event Name: " . $eventName . "  Payload: " . $payload;
-$logWrite2= fopen($logFile2, "a") or die("Unable to open file!");
-fwrite($logWrite2, date('Y-m-d h:i:s A',time()).": ".$data2."\n");
-fclose($logWrite2);
-
-
-#logEntry( "Event Name: " . $eventName . "  Payload: " . $payload);
+logEntry( "Event Name: " . $eventName . "  Payload: " . $payload);
 
 if (strlen($eventName) == 0){
 	throw new Exception('No event specified.');
@@ -24,8 +17,20 @@ if (strlen($payload) == 0){
 	throw new Exception('No payload specified.');
 }
 
-// if (strlen($name) == 0){
-// 	throw new Exception('No name specified.');
-// }
+$iftttKey = urldecode($pluginSettings['ifttt_key']);
+
+$isConfigured = false;
+if (strlen($pluginSettings['ifttt_key']) > 0)
+{
+	$isConfigured = true;
+}
+
+if ($isConfigured){
+	$payloads = getPayloadOptions();
+	
+}
+else{
+	logEntry("Skipping executing due to plugin not being configured");
+}
 
 ?>
