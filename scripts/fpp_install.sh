4cc045e25438343903408b3af2a462c4bb951117
Joe Harrison <joe.harrison@carpe.io>
2022-12-03 21:44:48 -0500

    Update fpp_install.sh

--- scripts/fpp_install.sh
+++ scripts/fpp_install.sh
@@ -2,14 +2,4 @@
 
 echo "Running fpp-ifttt-webhooks install script"
 
-# Mark to reboot
-BASEDIR=$(dirname $0)
-cd $BASEDIR
-cd ..
-make "SRCDIR=${SRCDIR}"
-
-
-. ${FPPDIR}/scripts/common
-setSetting rebootFlag 1
-
 #fpp_install
